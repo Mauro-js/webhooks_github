3ee00ec142b5d43da079638b8de0959562e7e426
Mauro Nievas <54127211+MauroNieva5@users.noreply.github.com>
2022-06-03 16:12:34 -0300

    test

--- server/main.js
+++ server/main.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended : false }))
 app.use(bodyParser.json());
 
 app.post('/payload', (req,res) => {
-    console.log("Holiiiis");
+    console.log(req.body);
     res.send(req.body);
 });
 
