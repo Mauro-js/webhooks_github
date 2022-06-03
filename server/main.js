e7e0a2bd917ed6b0516834d2293dae7ccdca3f28
Mauro Nievas <54127211+MauroNieva5@users.noreply.github.com>
2022-06-03 14:54:26 -0300

    express

--- server/main.js
+++ server/main.js
@@ -3,3 +3,23 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+
+'use strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = pocess.env.PORT || 4567
+
+app.use(bodyParser.urlencoded({ extended : false }))
+app.use(bodyParser.json());
+
+app.get('/payload', (req,res) => {
+    res.send(req);
+});
+
+app.listen(port, () => {
+    console.log(`API REST corriendo en http://localhost:${port}`);
+});
